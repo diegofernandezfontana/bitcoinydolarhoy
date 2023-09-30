f6568d5a81f4cbe5998fe8043a2084327bf6594f
Diego <diegofernandezfontana@gmail.com>
2023-09-30 17:30:02 +0200

    Auto update

--- start-docker.sh
+++ start-docker.sh
@@ -1,28 +1,40 @@
 #!/bin/sh
 
 
-#install expect to open ssh with user/pw
-apt-get install expect
 
-# Install bun (node already installed)
-curl -fsSL https://bun.sh/install | bash
+echo "-----START LS----------"
+ls -a $HOME
+echo "------END LS---------"
+
+export PATH=$PATH:/home/.bun/bin
 
 
 current_dir=$(pwd)
 
+echo "-----START LS----------"
 ls 
+echo "------END LS---------"
+
 #1- LOAD PRIVATE KEY
-eval "$(ssh-agent -s)"
-"$current_dir"/ssh_add.exp
+#eval "$(ssh-agent -s)"
+#"$current_dir"/ssh_add.exp
 
 #3 get historical data
 cd "$current_dir"/historical
 bun run get:all
 
-pnpm build
+#4 # get current price USD and BTC
+cd "$current_dir"/cron
+bun run ./src/index.ts
 
-# Ejecutar preview
+cp "$current_dir"/cron/src/output/last_update.json "$current_dir"/bitcoindolayhoy/src/last_update.json
+cp "$current_dir"/cron/src/output/last_update.json "$current_dir"/btcdolarhoy.tweeter/src/last_update.json
+
+cd "$current_dir/bitcoindolayhoy"
+
+
+pnpm build
 pnpm preview --host
 
-docker build -t btc-0.0.1 -f /home/diego/diego/Dockerfile . && docker run -p 8000:4321 btc-0.0.1
+#docker build -t btc-0.0.1 -f /home/diego/diego/Dockerfile . && docker run -p 8000:4321 btc-0.0.1
 
